70f29620e5a6cbbb56dade6c137ffe6c24ad7662
Victor Moraes <moraesvictorgk@gmail.com>
2021-04-14 09:32:47 -0300

    Header and Particles

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+// import { Container } from './styles';
+
+function Header() {
+  return(
+      <div className="header-wrapper">
+        <div className="main-info">
+          <h1>Web Development and WebSite Promotions</h1>
+          <Typed 
+          className="typed-text"
+          strings={["Web Design", "Web Development", "React JS", "Redux", "Hooks", "Software Developer"]}
+          typeSpeed={60}
+          backSpeed={60}
+          loop          
+          />
+          <a href="/" className="btn-main-offer">Contact me</a>
+        </div>
+      </div>
+  );
+}
+
+export default Header;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import logo from '../navBarLogo.png';
+// REACT FONT AWESOME IMPORT 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 // import { Container } from './styles';
 
 function NavBar() {
@@ -9,7 +12,7 @@ function NavBar() {
 
     <a className="navbar-brand" href="/"><img className="logo" src={ logo } alt="logo" height="70px"/>Victor Moraes</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+     < FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
     </button>
   
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
