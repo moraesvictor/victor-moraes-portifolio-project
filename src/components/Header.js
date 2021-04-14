76ac9d66970b05d6d8bc1ce7b105b0256b8d293c
Victor Moraes <moraesvictorgk@gmail.com>
2021-04-14 14:47:53 -0300

    change background cover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
 // import { Container } from './styles';
 
@@ -8,13 +8,14 @@ function Header() {
       <div className="header-wrapper">
         <div className="main-info">
           <h1>Web Development and WebSite Promotions</h1>
-          <Typed 
+          <h3>React JS | JavaScript | Redux | Hooks | Software Development</h3>
+          {/* <Typed 
           className="typed-text"
           strings={["Web Design", "Web Development", "React JS", "Redux", "Hooks", "Software Developer"]}
           typeSpeed={60}
           backSpeed={60}
           loop          
-          />
+          /> */}
           <a href="/" className="btn-main-offer">Contact me</a>
         </div>
       </div>
